b4cd4383b4c6925977fd5ad58e60b1e7ac0b5e6a
“Cassio-hue” <cassiocvtb2003@gmail.com>
2025-10-16 16:48:23 -0300

    feat: listar chats

--- components/ChatPersonCard.tsx
+++ components/ChatPersonCard.tsx
@@ -7,34 +7,53 @@ export interface ChatPersonCardProps {
   profileImageUrl: string;
   lastMessage: string;
   lastMessageTime: string;
-  onPress?: () => void;
+  onPress: () => void;
 }
 
-export default function ChatPersonCard(props: ChatPersonCardProps) {
-  const formatText = (text: string, maxLength: number) => {
-    if (text.length <= maxLength) return text;
-    return text.slice(0, maxLength) + '...';
-  };
+const formatText = (text: string, maxLength: number) => {
+  if (text.length <= maxLength) return text;
+  return text.slice(0, maxLength) + '...';
+};
 
+const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  const now = new Date();
+  const diffInMs = now.getTime() - date.getTime();
+  const diffInMinutes = Math.floor(diffInMs / 60000);
+
+  if (diffInMinutes < 1) return 'Agora';
+  if (diffInMinutes < 60) return `${diffInMinutes} min atrás`;
+  if (diffInMinutes < 1440) {
+    const hours = Math.floor(diffInMinutes / 60);
+    if (hours > 1) return `Há ${hours} horas`;
+    return `Há ${hours}h`;
+  }
+
+  const days = Math.floor(diffInMinutes / 1440);
+  if (days > 1) return `Há ${days} dias`;
+  return `Há ${days}dia`;
+};
+
+export default function ChatPersonCard(props: ChatPersonCardProps) {
   return (
     <TouchableOpacity style={styles.container} onPress={props.onPress}>
       <Image source={{ uri: props.profileImageUrl }} style={styles.userImage} />
       <View>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <View style={styles.dataContainer}>
           <View style={styles.nameContainer}>
             <Text numberOfLines={2} style={styles.name}>
               {props.name.toUpperCase()}
             </Text>
-            {props.nickname && (
-              <Text style={styles.name}>| {props.nickname.toUpperCase()}</Text>
-            )}
+            {/* {props.nickname && (
+              <Text style={styles.name}>|{props.nickname.toUpperCase()}</Text>
+            )} */}
           </View>
           <Text style={{ justifyContent: 'flex-end' }}>
-            {props.lastMessageTime}
+            {props.lastMessageTime && formatDate(props.lastMessageTime)}
           </Text>
         </View>
         <Text style={{ width: '80%' }}>
-          {formatText(props.lastMessage, 38)}
+          {props.lastMessage && formatText(props.lastMessage, 38)}
         </Text>
       </View>
     </TouchableOpacity>
@@ -64,6 +83,13 @@ const styles = StyleSheet.create({
   nameContainer: {
     flexDirection: 'row',
     gap: 8,
+    alignItems: 'center',
+  },
+  dataContainer: {
+    flex: 1,
+    width: '90%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
 });
